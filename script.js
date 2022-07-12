81bdb0b60765c82e46b881b1ed70b1974b3b9b27
Gr-Stanislav <grstas1990@gmail.com>
2022-07-12 20:58:06 +0300

    aded block acreditation

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-const links = document.querySelectorAll(".page-header ul a");
-
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
-
-  scroll({
-    top: offsetTop,
-    behavior: "smooth"
-  });
-}
-// const Head = document.querySelector('header');
-// const Icon = document.querySelector('.logo');
-// let Itom;
-// let WidthHead = Head.offsetWidth;
-// let HeighthHead = Head.offsetHeight;
-// console.log (WidthHead);
-
-// Icon.addEventListener('click', () => {
-//     let color = document.createElement('div');
-//     Head.appendChild(color);
-//     color.style.position = 'fixed';
-//     color.style.backgroundColor = 'yellow';
-//     color.style.padding = '3px';
-//     color.style.borderRadius = '50%';
-//     color.style.top = `14.5vh`;
-
-//     color.animate([
-//         {transform: 'translateX(0)'},
-//         {transform: `translateX(${WidthHead}px)`}
-//     ], {
-//         duration: 1000
-//     })
-    
-//     if (color.animate.transform == WidthHead){
-//         color.remove();
-//     }
-// });
-// import $ from 'jquery';
-// $('.car_sale_window').slick();
 
 $('.car_sale_window').slick({
     slidesToShow: 2,
@@ -58,7 +13,6 @@ const Item_FAQ = document.querySelectorAll('.item__p'),
       ItemText = document.querySelectorAll('.listing');
 
 let ImgFAQ = document.querySelectorAll('.item__p img');
-console.log(listing);
 
 
 function HiddenList (){
