196d113fa6af2ed3e9d07821d0c6463e64a426c0
Gr-Stanislav <grstas1990@gmail.com>
2022-07-22 21:46:40 +0300

    add new font-style

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $('.car_sale_window').slick({
 
 
 const Item_FAQ = document.querySelectorAll('.item__p'),
-      listing = document.querySelectorAll('.listing'),
+    //   listing = document.querySelectorAll('.FAQ_content__item ul'),
       ItemText = document.querySelectorAll('.listing');
 
 let ImgFAQ = document.querySelectorAll('.item__p img');
@@ -26,17 +26,19 @@ HiddenList();
 Item_FAQ.forEach((item, i) => {
     item.addEventListener('click', function(){
         ItemText[i].classList.toggle('close');
+        
+        
 
         if (!ItemText[i].classList.contains('close')){
             ImgFAQ[i].style.transition = '0.5s';
             ImgFAQ[i].style.transform = 'rotate(180deg)';
             ImgFAQ[i].src = "./images/minus.png";
+            // listing[i].style.transform = "translateX(-100%)";
         } else {
             ImgFAQ[i].style.transition = '0.5s';
             ImgFAQ[i].style.transform = 'rotate(90deg)';
             ImgFAQ[i].src = "./images/plus.png";
+            // listing[i].style.transform = "translateX(0%)";
         }
-
-
     });
 });
